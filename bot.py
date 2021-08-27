9dd2a262f0b2898dab3d5cfd5a59c46002c02cf8
Jack Pee <mcinvasion2016@gmail.com>
2021-08-26 22:45:19 -0700

    basic setup

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+import json
+import os
+
+# Load config
+with open("config.json", "r") as f:
+    config = json.load(f)
+
+# Initialize
+bot = commands.Bot(
+    command_prefix = config["prefix"]
+    )
+bot.remove_command("help")
+
+for file in os.listdir("./cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
+
+try:
+    bot.run(config["token"])
+except Exception as err:
+    print(f"Error: {err}")
