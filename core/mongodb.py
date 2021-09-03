dc9b3400c8aa229ac2d61f401a4e8f3570eb2417
Jack Pee <mcinvasion2016@gmail.com>
2021-09-02 21:08:09 -0700

    Finished MongoDB Basic Intergration

--- core/mongodb.py
+++ core/mongodb.py
@@ -12,8 +12,30 @@ dbPassword = config["dbPassword"]
 mongo = MongoClient(f"mongodb+srv://baka:{dbPassword}@akane.dsytm.mongodb.net/Kana?retryWrites=true&w=majority")
 
 database = mongo["Kana"]
-col = database["servers"]
+srv = database["servers"]
+usr = database["users"]
 
 def add_server(serverName, serverID):
-    col.insert_one({"serverid": serverID, "serverName": serverName, "manga": []})
+    srv.insert_one({"serverid": serverID, "serverName": serverName, "manga": []})
+
+def add_user(userName, userID):
+    usr.insert_one({"userid": userID, "userName": userName, "manga": []})
+
+def remove_server(serverID):
+    srv.delete_one({"serverid": serverID})
+
+def remove_user(userID):
+    usr.delete_one({"userid": userID})
+
+def add_manga(idName, title, mode):
+    if mode == "user":
+        usr.update_one({"userid": idName}, {"$push": {"manga": {"title": title}}})
+    elif mode == "server":
+        srv.update_one({"serverid": idName}, {"$push": {"manga": {"title": title}}})
+    
+def remove_manga(idName, title, mode):
+    if mode == "user":
+        usr.update_one({"userid": idName}, {"$pull": {"manga": {"title": title}}})
+    elif mode == "server":
+        srv.update_one({"serverid": idName}, {"$pull": {"manga": {"title": title}}})
 
