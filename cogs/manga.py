19dc1e9f69498de6751fa8b394a792566867e69c
Jack Pee <mcinvasion2016@gmail.com>
2021-09-12 00:38:50 -0700

    Finished initial setup command

--- cogs/manga.py
+++ cogs/manga.py
@@ -6,16 +6,23 @@ import threading
 import asyncio
 
 from core import update
+from core import mongodb
 
 class Manga(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    """
     async def notify(title, chapter, group, link):
-        if title == "Manga Not Found":
-            print("not a manga wanted lol")
+        serverWant = mongodb.manga_wanted(title, "server")
+        userWant = mongodb.manga_wanted(title, "user")
+        if serverWant == True:
+            
+        if userWant == True:
+            
         else:
-            for 
+            print("New manga not wanted lol")
+    """
 
     def checkForUpdates(bot):
         old = update.getLatest()
@@ -26,6 +33,45 @@ class Manga(commands.Cog):
                 for manga in newMangas:
                     asyncio.run_coroutine_threadsafe(notify(manga["title"], manga["chapter"], manga["group"], manga["link"]), bot.loop)
                 old = new
+
+    @commands.command()
+    async def setup(self, ctx):
+        await ctx.message.delete()
+        timeoutError = discord.Embed(title="Error", description="You didn't respond in time!", color=0xff4f4f)
+        setupEmbedS = discord.Embed(title="Setup", color=0x3083e3, description="Do you want manga updates sent to your DMs or a server? (Type user or server)")
+        sentEmbed = await ctx.send(embed=setupEmbedS)
+        try:
+            mode = await self.bot.wait_for('message', check=lambda x: x.author.id == ctx.author.id)
+        except asyncio.TimeoutError:
+            await sentEmbed.delete()
+            await ctx.send(embed=timeoutError, delete_after=5.0)
+        else:
+            await sentEmbed.delete()
+            await mode.delete()
+            if mode.content == "user":
+                embedUser = discord.Embed(title="Setup", color=0x3083e3, description="Great! You're all set up. Run the command +addmanga to add manga.")
+                await ctx.send(embed=embedUser, delete_after=10.0)
+            if mode.content == "server":
+                embedServer = discord.Embed(title="Setup", color=0x3083e3, description="What channel should I use?")
+                sentEmbedServer = await ctx.send(embed=embedServer)
+                try:
+                    channel = await self.bot.wait_for('message', check=lambda x: x.author.id == ctx.author.id)
+                except asyncio.TimeoutError:
+                    await sentEmbedServer.delete()
+                    await ctx.send(embed=timeoutError, delete_after=5.0)
+                else:
+                    await sentEmbedServer.delete()
+                    await channel.delete()
+                    serverid = ctx.message.guild.id
+                    if mongodb.checkServerExist(serverid) == True:
+                        alrfinishSS = discord.Embed(title="Setup", color=0x3083e3, description="This server is already setup. Run the command +addmanga to add manga.")
+                        await ctx.send(embed=alrfinishSS, delete_after=10.0)
+                    elif mongodb.checkServerExist(serverid) == False:
+                        channelid = channel.channel_mentions[0].id
+                        serverInfo = self.bot.get_guild(serverid)
+                        mongodb.addServer(serverInfo.name, serverid, channelid)
+                        finishServerSetup = discord.Embed(title="Setup", color=0x3083e3, description="Great! You're all set up. Run the command +addmanga to add manga.")
+                        await ctx.send(embed=finishServerSetup, delete_after=10.0)
 
 def setup(bot):
     bot.add_cog(Manga(bot))
--- core/mongodb.py
+++ core/mongodb.py
@@ -15,27 +15,56 @@ database = mongo["Kana"]
 srv = database["servers"]
 usr = database["users"]
 
-def add_server(serverName, serverID):
-    srv.insert_one({"serverid": serverID, "serverName": serverName, "manga": []})
+def addServer(serverName, serverID, channelID):
+    srv.insert_one({"serverid": serverID, "serverName": serverName, "channelid": channelID, "manga": []})
 
-def add_user(userName, userID):
+def addUser(userName, userID):
     usr.insert_one({"userid": userID, "userName": userName, "manga": []})
 
-def remove_server(serverID):
+def removeServer(serverID):
     srv.delete_one({"serverid": serverID})
 
-def remove_user(userID):
+def removeUser(userID):
     usr.delete_one({"userid": userID})
 
-def add_manga(idName, title, mode):
+def updateChannel(idName, channelID):
+    srv.update_one({"serverid": idName}, {"$set": {"channelid": channelID}})
+
+def checkServerExist(idName):
+    result = srv.find_one({"serverid": idName})
+    if result != None:
+        return True
+    else:
+        return False
+
+def addManga(idName, title, mode):
     if mode == "user":
         usr.update_one({"userid": idName}, {"$push": {"manga": {"title": title}}})
     elif mode == "server":
         srv.update_one({"serverid": idName}, {"$push": {"manga": {"title": title}}})
     
-def remove_manga(idName, title, mode):
+def removeManga(idName, title, mode):
     if mode == "user":
         usr.update_one({"userid": idName}, {"$pull": {"manga": {"title": title}}})
     elif mode == "server":
         srv.update_one({"serverid": idName}, {"$pull": {"manga": {"title": title}}})
 
+def mangaWanted(title, mode):
+    if mode == "user":
+        result = srv.find_one({"manga": {"$elemMatch": {"title": title}}})
+    elif mode == "server":
+        result = srv.find_one({"manga": {"$elemMatch": {"title": title}}})
+
+    if result != None:
+        return True
+    else:
+        return False
+
+def test(title):
+    sheeesh = []
+    for document in srv.find():
+        print(document[2])
+
+#        if result != None:
+#            sheeesh.append(result.serverid)
+#    return result
