829cc7a463dcee24536930405805e97674416261
ohashi <notjackhli@gmail.com>
2021-09-22 08:14:03 -0700

    Close requests after each connection

--- core/update.py
+++ core/update.py
@@ -41,7 +41,10 @@ def getLatest():
 
 # Get image from mangaupdates
 def getImage(link):
-    websiteResult = requests.get(link, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
+    websiteResult = requests.get(link, headers={
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        "Connection":"close"
+        })
     htmlData = websiteResult.text
     soup = bs(htmlData, "html.parser") 
     for img in soup.find_all("img"):
@@ -50,7 +53,10 @@ def getImage(link):
     return image
 
 def getTitle(link):
-    websiteResult = requests.get(link, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
+    websiteResult = requests.get(link, headers={
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        "Connection":"close"
+        })
     htmlData = websiteResult.text
     soup = bs(htmlData, "html.parser")
     text = soup.find("span", {"class": "releasestitle tabletitle"})
@@ -58,7 +64,10 @@ def getTitle(link):
     return title
 
 def getAllTitles(link, title):
-    websiteResult = requests.get(link, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
+    websiteResult = requests.get(link, headers={
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        "Connection":"close"
+        })
     htmlData = websiteResult.text
     soup = bs(htmlData, "html.parser")
     table = soup.findAll('div')
@@ -80,7 +89,10 @@ def getAllTitles(link, title):
         i += 1
 
 def getLink(title):
-    websiteResult = requests.post("https://mangaupdates.com/search.html", params={"search": title}, headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"})
+    websiteResult = requests.post("https://mangaupdates.com/search.html", params={"search": title}, headers={
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+        "Connection":"close"
+        })
     htmlData = websiteResult.text
     soup = bs(htmlData, "html.parser")
     table = soup.find_all('a', {"alt": "Series Info"})
