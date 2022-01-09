86e5abdcb30df7f3dc6dac27f5cc05480c3f34c6
Jack Pee <mcinvasion2016@gmail.com>
2022-01-09 00:48:21 -0700

    style: fix formatting in footers & add text to alert

--- cogs/info.py
+++ cogs/info.py
@@ -40,7 +40,7 @@ class Information(commands.Cog):
         embed.add_field(name="+deleteaccount `user/server`", value="Deletes your account and your manga list.", inline=False)
         embed.add_field(name="+setgroup `user/server`", value="Sets a manga's scan group. Only that scan group's chapter updates for that manga will be sent.", inline=False)
         embed.add_field(name="+search `manga`", value="Searches for information about a manga.", inline=False)
-        embed.set_footer(text="URGENT - PLEASE READ: +alert")
+        embed.set_footer(text="PLEASE READ (server owners): +alert")
         await ctx.send(embed=embed)
 
     @commands.command(name="ping")
@@ -84,13 +84,13 @@ class Information(commands.Cog):
         embed = discord.Embed(title="Source Code", color=0x3083e3, description="MangaUpdate's source code can be found on GitHub. Any issues with the bot can be raised there.")
         embed.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
         embed.add_field(name="Link", value="https://github.com/ohashizu/mangaupdates-bot")
-        embed.set_footer(text="URGENT - PLEASE READ: +alert")
+        embed.set_footer(text="PLEASE READ (server owners): +alert")
         await ctx.send(embed=embed)
 
     @commands.command(name="alert")
     async def alert(self, ctx):
         link = 'https://discord.com/oauth2/authorize?client_id=880694914365685781&scope=applications.commands%20bot&permissions=268856384'
-        description = f"Yo everyone! Recently Discord changed their API to require message content as intents. They want every bot to move to slash commands. This means that this bot needs new permissions to use these slash commands (don't ask me why).\n\nPlease reinvite the bot with the link or else by April 30, 2022, you won't be able to use the bot. Thanks for understanding and using MangaUpdates Bot!"
+        description = f"*This alert is only if you invited the bot before December 25th.*\n\nYo everyone! Recently Discord changed their API to require message content as intents. They want every bot to move to slash commands. This means that this bot needs new permissions to use these slash commands (don't ask me why).\n\nPlease reinvite the bot with the link or else by April 30, 2022, you won't be able to use the bot. Thanks for understanding and using MangaUpdates Bot!"
         embed = discord.Embed(title="Alert - Please read", color=0x3083e3, description=description)
         embed.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
         embed.set_footer(text="If button doesn't work: https://discord.com/oauth2/authorize?client_id=880694914365685781&scope=applications.commands%20bot&permissions=268856384")
