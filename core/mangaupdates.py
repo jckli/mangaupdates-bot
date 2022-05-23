9c2b8802140e2f35d8acbe5c7b8351ab0fbeb079
Jack Li <jack@jackli.dev>
2022-05-23 00:58:28 -0700

    chore: async functions

--- core/mangaupdates.py
+++ core/mangaupdates.py
@@ -11,16 +11,16 @@ class MangaUpdates:
         self.token = login["context"]["session_token"]
         self.headers = {"Authorization": f"Bearer {self.token}"}
 
-    def convert_old_id(self, old_id):
+    async def convert_old_id(self, old_id):
         enc = numpy.base_repr(old_id, 36).lower()
         return enc
     
-    def search_series(self, series_name):
+    async def search_series(self, series_name):
         searchurl = f"https://api.mangaupdates.com/v1/series/search"
         search = requests.post(searchurl, data={"search": series_name, "perpage": 10}, headers=self.headers).json()
         return search
     
-    def series_info(self, series_id):
+    async def series_info(self, series_id):
         infourl = f"https://api.mangaupdates.com/v1/series/{series_id}"
         info = requests.get(infourl, headers=self.headers).json()
         return info
