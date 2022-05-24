4eaf2e9dc4b2c8358bf8c5ebf038963753c46b53
Jack Li <jack@jackli.dev>
2022-05-23 18:09:46 -0700

    feat: finished search command

--- cogs/manga_general.py
+++ cogs/manga_general.py
@@ -4,11 +4,12 @@ from discord.commands import Option, slash_command
 import validators
 import os
 from core.mangaupdates import MangaUpdates
+from core.manga_util import SearchData
 
 mangaupdates = MangaUpdates()
 ghuser = os.environ.get("GITHUB_USER")
 
-timeoutError = discord.Embed(title="Error", description="You didn't respond in time!", color=0xff4f4f)
+timeoutError = discord.Embed(title="Error", description="You didn't respond in time! Please rerun the command.", color=0xff4f4f)
 
 # todo: make dropdown only work once, also make the timeout stop showing if it didnt time out (think it already does but dunno)
 class SelectMangaView(discord.ui.View):
@@ -35,40 +36,19 @@ class SelectManga(discord.ui.Select):
         value = int(self.values[0][:2].replace(".", "")) - 1
         mangaid = self.manga_list[value]["id"]
         series_info = await mangaupdates.series_info(mangaid)
-        title = series_info["title"]
-        description = series_info["description"]
-        if series_info["completed"] is True:
-            status = "Completed"
-        else:
-            status = "Ongoing"
-        image = series_info["image"]["url"]["original"]
-        url = series_info["url"]
-        mangatype = series_info["type"]
-        year = series_info["year"]
-        latest_chapter = series_info["latest_chapter"]
-        rating = series_info["bayesian_rating"]
-        authorsList = []
-        artistsList = []
-        for author in series_info["authors"]:
-            if author["type"] == "Author":
-                authorsList.append(author["name"])
-            if author["type"] == "Artist":
-                artistsList.append(author["name"])
-        authors = ", ".join(authorsList)
-        artists = ", ".join(artistsList)
-
-        result = discord.Embed(title=title, url=url, color=0x3083e3, description=description)
-        if image != None:
-            result.set_image(url=image)
+        data = SearchData(series_info)
+        result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
+        if data.image != None:
+            result.set_image(url=data.image)
         result.set_author(name="MangaUpdates", icon_url=interaction.client.user.avatar.url)
         result.set_footer(text=f"Requested by {interaction.user.name}", icon_url=interaction.user.display_avatar)
-        result.add_field(name="Year", value=year, inline=True)
-        result.add_field(name="Type", value=mangatype, inline=True)
-        result.add_field(name="Latest Chapter", value=latest_chapter, inline=True)
-        result.add_field(name="Author(s)", value=authors, inline=True)
-        result.add_field(name="Artist(s)", value=artists, inline=True)
-        result.add_field(name="Rating", value=rating, inline=True)
-        await interaction.response.send_message(embed=result)
+        result.add_field(name="Year", value=data.year, inline=True)
+        result.add_field(name="Type", value=data.mangatype, inline=True)
+        result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
+        result.add_field(name="Author(s)", value=data.authors, inline=True)
+        result.add_field(name="Artist(s)", value=data.artists, inline=True)
+        result.add_field(name="Rating", value=data.rating, inline=True)
+        await interaction.response.edit_message(embed=result, view=None)
         self.finish = True
 
 class MangaGeneral(commands.Cog):
@@ -76,10 +56,25 @@ class MangaGeneral(commands.Cog):
         self.bot = bot
 
     @slash_command(name="search", description="Searches for a manga series", guild_ids=[721216108668911636])
-    async def search(self, ctx, manga):
+    async def search(self, ctx, manga: Option(str, description="The name of the manga series (can use mangaupdates links)", required=True)):
         if validators.url(manga) is True:
             link = manga.partition("https://www.mangaupdates.com/series/")[2]
             mangaid = link.partition("/")[0]
+            mangaid = await mangaupdates.convert_new_id(mangaid)
+            series_info = await mangaupdates.series_info(mangaid)
+            data = SearchData(series_info)
+            result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
+            if data.image != None:
+                result.set_image(url=data.image)
+            result.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
+            result.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.display_avatar)
+            result.add_field(name="Year", value=data.year, inline=True)
+            result.add_field(name="Type", value=data.mangatype, inline=True)
+            result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
+            result.add_field(name="Author(s)", value=data.authors, inline=True)
+            result.add_field(name="Artist(s)", value=data.artists, inline=True)
+            result.add_field(name="Rating", value=data.rating, inline=True)
+            await ctx.respond(embed=result)
         elif validators.url(manga) is not True:
             await ctx.defer()
             search_results = []
@@ -101,11 +96,10 @@ class MangaGeneral(commands.Cog):
                     count += 1
                 searchEmbed = discord.Embed(title="Search Results", color=0x3083e3, description=description)
                 manga_drop = SelectMangaView(manga_list=search_results)
-                await ctx.respond(embed=searchEmbed, view=manga_drop)
+                search = await ctx.respond(embed=searchEmbed, view=manga_drop)
                 await manga_drop.wait()
-                manga_drop.disable_all_items()
                 if manga_drop.select_manga.finish is None:
-                    await ctx.respond(embed=timeoutError)
+                    await search.edit(embed=timeoutError, view=None)
                     return
                 else:
                     return
