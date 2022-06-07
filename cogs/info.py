51a387a997f86247fe61167aa24db0ddb41534e5
Jack Li <jack@jackli.dev>
2022-06-06 18:49:01 -0700

    feat: add mangaupdates info command

--- cogs/info.py
+++ cogs/info.py
@@ -1,11 +1,22 @@
 import discord
 from discord.ext import commands
 from discord.commands import Option, slash_command
+import time
+from datetime import datetime
+from datetime import timedelta
+
+startTime = time.time()
 
 class Link(discord.ui.View):
     def __init__(self, label, link):
         super().__init__()
         self.add_item(discord.ui.Button(label=label, url=link))
+
+class InfoButtons(discord.ui.View):
+    def __init__(self, support_server, github):
+        super().__init__()
+        self.add_item(discord.ui.Button(label="Support Server", url=support_server))
+        self.add_item(discord.ui.Button(label="GitHub", url=github))
 
 class Information(commands.Cog):
     def __init__(self, bot):
@@ -30,6 +41,24 @@ class Information(commands.Cog):
         embed.add_field(name="setgroup `user/server`", value="Sets a manga's scan group. Only that scan group's chapter updates for that manga will be sent.", inline=False)
         embed.add_field(name="search `manga`", value="Searches for information about a manga.", inline=False)
         await ctx.respond(embed=embed, ephemeral=True)
+
+    @slash_command(name="mangaupdates", description="Displays basic information about MangaUpdates", guild_ids=[721216108668911636])
+    async def mangaupdates(self, ctx):
+        activeServers = self.bot.guilds
+        botUsers = 0
+        for i in activeServers:
+            botUsers += i.member_count
+        currentTime = time.time()
+        differenceUptime = int(round(currentTime - startTime))
+        uptime = str(timedelta(seconds = differenceUptime))
+        botinfo = discord.Embed(
+            title="MangaUpdates",
+            color=0x3083e3,
+            timestamp=datetime.now(),
+            description=f"Thanks for using MangaUpdates bot! Any questions can be brought up in the support server. This bot is also open-source! All code can be found on GitHub (Please leave a star ⭐ if you enjoy the bot).\n\n**Server Count:** {len(self.bot.guilds)}\n**Bot Users:** {botUsers}\n**Bot Uptime:** {uptime}"
+        )
+        botinfo.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
+        await ctx.respond(embed=botinfo, view=InfoButtons("https://discord.gg/Fr2BhuCkET", "https://github.com/jckli/mangaupdates-bot"))
 
     @slash_command(name="ping", description="Pong!", guild_ids=[721216108668911636])
     async def ping(self, ctx):
