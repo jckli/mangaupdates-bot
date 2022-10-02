1b56af09c242ecca19b2e3c03177f3fab3f5d75f
lina <jack@jackli.dev>
2022-10-01 22:58:18 -0700

    refactor: move manga search to under manga group
    more organized if other types implemented

--- cogs/manga_general.py
+++ cogs/manga_general.py
@@ -5,7 +5,7 @@ import validators
 import os
 from core.mongodb import Mongo
 from core.mangaupdates import MangaUpdates
-from core.manga_util import SearchData
+
 
 mongo = Mongo()
 mangaupdates = MangaUpdates()
@@ -49,46 +49,6 @@ class Confirm(discord.ui.View):
         self.interaction = interaction
         self.stop()
 
-# search command views
-class SelectMangaView(discord.ui.View):
-    def __init__(self, manga_list):
-        super().__init__(timeout=15.0)
-        self.select_manga = SelectManga(manga_list=manga_list)
-        self.add_item(self.select_manga)
-
-class SelectManga(discord.ui.Select):
-    def __init__(self, manga_list):
-        manga_desc = []
-        for manga in manga_list:
-            manga_desc.append(discord.SelectOption(label=manga["name"], description=manga["description"]))
-        super().__init__(
-            placeholder="Choose a manga series...",
-            min_values=1,
-            max_values=1,
-            options=manga_desc
-        )
-        self.manga_list = manga_list
-        self.finish = None
-
-    async def callback(self, interaction: discord.Interaction):
-        value = int(self.values[0][:2].replace(".", "")) - 1
-        mangaid = self.manga_list[value]["id"]
-        series_info = await mangaupdates.series_info(mangaid)
-        data = SearchData(series_info)
-        result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
-        if data.image != None:
-            result.set_image(url=data.image)
-        result.set_author(name="MangaUpdates", icon_url=interaction.client.user.avatar.url)
-        result.set_footer(text=f"Requested by {interaction.user.name}", icon_url=interaction.user.display_avatar)
-        result.add_field(name="Year", value=data.year, inline=True)
-        result.add_field(name="Type", value=data.mangatype, inline=True)
-        result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
-        result.add_field(name="Author(s)", value=data.authors, inline=True)
-        result.add_field(name="Artist(s)", value=data.artists, inline=True)
-        result.add_field(name="Rating", value=data.rating, inline=True)
-        await interaction.response.edit_message(embed=result, view=None)
-        self.finish = True
-
 # Helpers
 async def is_admin(ctx):
     has_add_permission = ctx.author.guild_permissions.administrator
@@ -110,63 +70,6 @@ async def validate_admin_and_server(ctx):
 class MangaGeneral(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @slash_command(name="search", description="Searches for a manga series")
-    async def search(self, ctx, manga: Option(str, description="The name of the manga series (can use mangaupdates links)", required=True)):
-        if validators.url(manga) is True:
-            link = manga.partition("https://www.mangaupdates.com/series/")[2]
-            mangaid = link.partition("/")[0]
-            mangaid = await mangaupdates.convert_new_id(mangaid)
-            series_info = await mangaupdates.series_info(mangaid)
-            data = SearchData(series_info)
-            result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
-            if data.image != None:
-                result.set_image(url=data.image)
-            result.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
-            result.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.display_avatar)
-            result.add_field(name="Year", value=data.year, inline=True)
-            result.add_field(name="Type", value=data.mangatype, inline=True)
-            result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
-            result.add_field(name="Author(s)", value=data.authors, inline=True)
-            result.add_field(name="Artist(s)", value=data.artists, inline=True)
-            result.add_field(name="Rating", value=data.rating, inline=True)
-            await ctx.respond(embed=result)
-        elif validators.url(manga) is not True:
-            await ctx.defer()
-            search_results = []
-            description = "Type the number of the manga you want to see information for.\n"
-            search_data = await mangaupdates.search_series(manga)
-            if search_data["results"] == []:
-                resultError = discord.Embed(title="Error", color=0xff4f4f, description="No mangas were found.")
-                await ctx.respond(embed=resultError)
-                return
-            elif search_data["results"] != []:
-                count = 1
-                for manga_data in search_data["results"]:
-                    manga = manga_data["record"]
-                    name = manga["title"]
-                    year = manga["year"]
-                    rating = manga["bayesian_rating"]
-                    description += f"{count}. {name} ({year}, Rating: {rating})\n"
-                    search_results.append({"id": manga["series_id"], "name": f"{count}. {name}", "description": f"{year}, Rating: {rating}", "info": manga})
-                    count += 1
-                searchEmbed = discord.Embed(title="Search Results", color=0x3083e3, description=description)
-                manga_drop = SelectMangaView(manga_list=search_results)
-                search = await ctx.respond(embed=searchEmbed, view=manga_drop)
-                await manga_drop.wait()
-                if manga_drop.select_manga.finish is None:
-                    await search.edit(embed=timeoutError, view=None)
-                    return
-                else:
-                    return
-            else:
-                completeError = discord.Embed(title="Error", color=0xff4f4f, description=f"Something went wrong. Create an issue here for support: https://github.com/{ghuser}/mangaupdates-bot")
-                await ctx.respond(embed=completeError)
-                return
-        else:
-            completeError = discord.Embed(title="Error", color=0xff4f4f, description=f"Something went wrong. Create an issue here for support: https://github.com/{ghuser}/mangaupdates-bot")
-            await ctx.respond(embed=completeError)
-            return
 
     server = SlashCommandGroup("server", description="Server commands", guild_only=True)
 
--- cogs/manga_main.py
+++ cogs/manga_main.py
@@ -5,6 +5,7 @@ from discord.commands import Option, SlashCommandGroup
 from core.mongodb import Mongo
 from core.mangaupdates import MangaUpdates
 from core.utils import Util
+from core.manga_util import SearchData
 import validators
 
 mangaupdates = MangaUpdates()
@@ -50,14 +51,54 @@ class Confirm(discord.ui.View):
         self.interaction = interaction
         self.stop()
 
-# manga add views
-class SelectMangaView(discord.ui.View):
-    def __init__(self, manga_list, mode):
+# search command views
+class SelectMangaSearchView(discord.ui.View):
+    def __init__(self, manga_list):
         super().__init__(timeout=15.0)
-        self.select_manga = SelectMangaWConfirm(manga_list=manga_list, mode=mode)
+        self.select_manga = SelectMangaSearch(manga_list=manga_list)
         self.add_item(self.select_manga)
 
-class SelectMangaWConfirm(discord.ui.Select):
+class SelectMangaSearch(discord.ui.Select):
+    def __init__(self, manga_list):
+        manga_desc = []
+        for manga in manga_list:
+            manga_desc.append(discord.SelectOption(label=manga["name"], description=manga["description"]))
+        super().__init__(
+            placeholder="Choose a manga series...",
+            min_values=1,
+            max_values=1,
+            options=manga_desc
+        )
+        self.manga_list = manga_list
+        self.finish = None
+
+    async def callback(self, interaction: discord.Interaction):
+        value = int(self.values[0][:2].replace(".", "")) - 1
+        mangaid = self.manga_list[value]["id"]
+        series_info = await mangaupdates.series_info(mangaid)
+        data = SearchData(series_info)
+        result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
+        if data.image != None:
+            result.set_image(url=data.image)
+        result.set_author(name="MangaUpdates", icon_url=interaction.client.user.avatar.url)
+        result.set_footer(text=f"Requested by {interaction.user.name}", icon_url=interaction.user.display_avatar)
+        result.add_field(name="Year", value=data.year, inline=True)
+        result.add_field(name="Type", value=data.mangatype, inline=True)
+        result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
+        result.add_field(name="Author(s)", value=data.authors, inline=True)
+        result.add_field(name="Artist(s)", value=data.artists, inline=True)
+        result.add_field(name="Rating", value=data.rating, inline=True)
+        await interaction.response.edit_message(embed=result, view=None)
+        self.finish = True
+
+# manga add views
+class SelectMangaAddView(discord.ui.View):
+    def __init__(self, manga_list, mode):
+        super().__init__(timeout=15.0)
+        self.select_manga = SelectMangaAddWConfirm(manga_list=manga_list, mode=mode)
+        self.add_item(self.select_manga)
+
+class SelectMangaAddWConfirm(discord.ui.Select):
     def __init__(self, manga_list, mode):
         manga_desc = []
         for manga in manga_list:
@@ -261,6 +302,63 @@ class MangaMain(commands.Cog):
     
     manga = SlashCommandGroup("manga", "Manga related commands")
 
+    @manga.command(name="search", description="Searches for a manga series")
+    async def search(self, ctx, manga: Option(str, description="The name of the manga series (can use mangaupdates links)", required=True)):
+        if validators.url(manga) is True:
+            link = manga.partition("https://www.mangaupdates.com/series/")[2]
+            mangaid = link.partition("/")[0]
+            mangaid = await mangaupdates.convert_new_id(mangaid)
+            series_info = await mangaupdates.series_info(mangaid)
+            data = SearchData(series_info)
+            result = discord.Embed(title=f"{data.title} ({data.status})", url=data.url, color=0x3083e3, description=data.description)
+            if data.image != None:
+                result.set_image(url=data.image)
+            result.set_author(name="MangaUpdates", icon_url=self.bot.user.avatar.url)
+            result.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.display_avatar)
+            result.add_field(name="Year", value=data.year, inline=True)
+            result.add_field(name="Type", value=data.mangatype, inline=True)
+            result.add_field(name="Latest Chapter", value=data.latest_chapter, inline=True)
+            result.add_field(name="Author(s)", value=data.authors, inline=True)
+            result.add_field(name="Artist(s)", value=data.artists, inline=True)
+            result.add_field(name="Rating", value=data.rating, inline=True)
+            await ctx.respond(embed=result)
+        elif validators.url(manga) is not True:
+            await ctx.defer()
+            search_results = []
+            description = "Type the number of the manga you want to see information for.\n"
+            search_data = await mangaupdates.search_series(manga)
+            if search_data["results"] == []:
+                resultError = discord.Embed(title="Error", color=0xff4f4f, description="No mangas were found.")
+                await ctx.respond(embed=resultError)
+                return
+            elif search_data["results"] != []:
+                count = 1
+                for manga_data in search_data["results"]:
+                    manga = manga_data["record"]
+                    name = manga["title"]
+                    year = manga["year"]
+                    rating = manga["bayesian_rating"]
+                    description += f"{count}. {name} ({year}, Rating: {rating})\n"
+                    search_results.append({"id": manga["series_id"], "name": f"{count}. {name}", "description": f"{year}, Rating: {rating}", "info": manga})
+                    count += 1
+                searchEmbed = discord.Embed(title="Search Results", color=0x3083e3, description=description)
+                manga_drop = SelectMangaSearchView(manga_list=search_results)
+                search = await ctx.respond(embed=searchEmbed, view=manga_drop)
+                await manga_drop.wait()
+                if manga_drop.select_manga.finish is None:
+                    await search.edit(embed=timeoutError, view=None)
+                    return
+                else:
+                    return
+            else:
+                completeError = discord.Embed(title="Error", color=0xff4f4f, description=f"Something went wrong. Create an issue here for support: https://github.com/{ghuser}/mangaupdates-bot")
+                await ctx.respond(embed=completeError)
+                return
+        else:
+            completeError = discord.Embed(title="Error", color=0xff4f4f, description=f"Something went wrong. Create an issue here for support: https://github.com/{ghuser}/mangaupdates-bot")
+            await ctx.respond(embed=completeError)
+            return
+
     @manga.command(name="add", description="Adds manga to your list to be tracked")
     async def add(self, ctx, manga: Option(str, description="The name of the manga series (can use mangaupdates links)", required=True)):
         if ctx.guild is not None:
@@ -355,7 +453,7 @@ class MangaMain(commands.Cog):
                     search_results.append({"id": manga["series_id"], "name": f"{count}. {name}", "description": f"{year}, Rating: {rating}", "info": manga})
                     count += 1
                 searchEmbed = discord.Embed(title="Search Results", color=0x3083e3, description=description)
-                manga_drop = SelectMangaView(manga_list=search_results, mode=modeval)
+                manga_drop = SelectMangaAddView(manga_list=search_results, mode=modeval)
                 if mode is not None:
                     await mode.interaction.response.edit_message(embed=searchEmbed, view=manga_drop)
                 else:
