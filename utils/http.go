0ec4b6049bb7a657a8c91e5101796e1eb45916e6
jckli <me@jackli.dev>
2026-01-04 21:36:16 +0000

    chore: remove debug prints

--- utils/http.go
+++ utils/http.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/valyala/fasthttp"
 )
 
@@ -27,7 +26,6 @@ func (c *Client) Get(endpoint string) ([]byte, int, error) {
 	defer fasthttp.ReleaseRequest(req)
 
 	url := c.BaseURL + endpoint
-	fmt.Println(url)
 
 	req.Header.SetMethod("GET")
 	req.SetRequestURI(url)
